ff090323ed9271a561b5fa0b7d8a3f7ec8ca1680
oBAllOlo <sittipongj74@gmail.com>
2024-09-02 07:32:49 +0700

    first commit

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,28 @@
+import bcrypt from "bcrypt";
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({ name, email, password });
+
+    // authenticate
+
+    res.status(201).json({ user, message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  res.send("Login route called");
+};
+
+export const logout = async (req, res) => {
+  res.send("Logout route called");
+};
