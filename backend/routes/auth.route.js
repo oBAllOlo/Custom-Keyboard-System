070b243c03daf83c9c8f9e9a758a092ec018ecd0
oBAllOlo <sittipongj74@gmail.com>
2024-09-03 01:52:33 +0700

    add refreshToken

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controllers.js";
+import {
+  login,
+  logout,
+  signup,
+  refreshToken,
+} from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/refresh-token", refreshToken);
 
 export default router;
